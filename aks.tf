7a058e0354c32be2b9cb1d52406490c202525d16
Máté Eckl <ecklm94@gmail.com>
2026-08-31 18:58:20 +0200

    aks node count increase

--- aks.tf
+++ aks.tf
@@ -15,7 +15,7 @@ resource "azurerm_kubernetes_cluster" "this" {
 
   default_node_pool {
     name       = "system"
-    node_count = 1
+    node_count = 2
     vm_size    = "Standard_D2as_v5"
   }
 
